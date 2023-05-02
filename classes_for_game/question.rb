96e5221232127e8cf63f3030c8350d480aa9ce5f
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-01 20:34:06 -0400

    added * and / functionality

--- classes_for_game/question.rb
+++ classes_for_game/question.rb
@@ -3,16 +3,17 @@ class Question
   def initialize()
     @first_num = rand(1..20)
     @second_num = rand(1..20)
-    @operation = ["+", "-"]
-    @index = rand(2)
+    @operation = ["+", "-", "*", "/"]
+    @index = rand(4)
   end
 
   def question(current_player)
-    puts "#{current_player}: What does #{@first_num} #{@operation[@index]} #{@second_num} equal?"
+    puts "#{current_player}: What does #{@first_num} #{@operation[@index]} #{@second_num} equal to the nearest integer?"
   end
 
   def answer
-    eval "#{@first_num} #{@operation[@index]} #{@second_num}"
+    result = eval "#{@first_num} #{@operation[@index]} #{@second_num}"
+    result.round()
   end
 
 end
